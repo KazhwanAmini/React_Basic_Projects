f490a2610ad4c774ba4477e40563b2fe29bdbe24
KazhwanAmini <kazh.amini@gmail.com>
2023-08-17 04:05:38 +0330

    Sort By Age

--- Birthday Reminder/src/List.js
+++ Birthday Reminder/src/List.js
@@ -3,19 +3,21 @@ import React from 'react'
 const List = ({ people }) => {
   return (
     <>
-      {people.map((person) => {
-        const { id, name, age, image, gender } = person
-        return (
-          <article key={id} className='person'>
-            <img className={gender} src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{age} years</p>
-              <p>{gender}</p>
-            </div>
-          </article>
-        )
-      })}
+      {people
+        .sort((prev, next) => next.age - prev.age)
+        .map((person) => {
+          const { id, name, age, image, gender } = person
+          return (
+            <article key={id} className='person'>
+              <img className={gender} src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+                <p>{gender}</p>
+              </div>
+            </article>
+          )
+        })}
     </>
   )
 }
