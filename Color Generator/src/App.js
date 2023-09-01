7d26b8c721af439468915f3b05db54ca21f7f2f9
KazhwanAmini <kazh.amini@gmail.com>
2023-09-01 22:02:20 +0330

    Color Gen(Done)

--- Color Generator/src/App.js
+++ Color Generator/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,20 +27,25 @@ function App() {
           <input
             type='text'
             value={color}
-            onChange={(e) => {
-              setColor(e.target.value)
-            }}
+            onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025'
             className={`${error ? 'error' : null}`}
           />
-          <button type='submit' className='btn'>
-            generate
+          <button className='btn' type='submit'>
+            submit
           </button>
         </form>
       </section>
       <section className='colors'>
         {list.map((color, index) => {
-          return <SingleColor key={index} {...color} index={index} />
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
         })}
       </section>
     </>
