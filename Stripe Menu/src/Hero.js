35bf698d848556a17ae3c4c7419ee445e9c3652b
KazhwanAmini <kazh.amini@gmail.com>
2023-09-15 21:52:27 +0330

    Stripe Menu Progress

--- Stripe Menu/src/Hero.js
+++ Stripe Menu/src/Hero.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { closeSubmenu } = useGlobalContext()
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payments infrastructure for the interner</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore,
+            alias saepe ducimus nesciunt at, itaque officia inventore laudantium
+            atque aspernatur nisi et laboriosam dolorem ullam!
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
